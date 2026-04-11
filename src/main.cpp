8b7ab8713d9f7e5c30ed7fcea3567f520470da8e
Shiroi <vadeev.dima@mail.ru>
2026-04-11 15:32:14 +0300

    feat: created the configuration loader

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "parsers/configurationloader.hpp"
+
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -9,6 +11,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     
+    parsers::ConfigurationLoader loader;
+    if(!loader.read("cfg.json")) { return -1; };
+
+    
+
     cout << "Hello World!" << endl;
     return 0;
 }
